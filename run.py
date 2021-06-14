c4c86ec232f04c2f1c87e1a740e3233ba0cfe47a
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-06-14 22:46:19 +0200

    Remove horizontal and add Rotation

--- run.py
+++ run.py
@@ -41,7 +41,8 @@ def get_dataset(opts):
     """
     train_transform = transform.Compose([
         transform.RandomResizedCrop(opts.crop_size, (0.5, 2.0)),
-        transform.RandomHorizontalFlip(),
+        transform.RandomRotation(degrees=45),
+        #transform.RandomHorizontalFlip(),
         transform.ToTensor(),
         transform.Normalize(mean=[0.485, 0.456, 0.406],
                             std=[0.229, 0.224, 0.225]),
