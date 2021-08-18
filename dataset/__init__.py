e6396b0fa56b177e88f3d412953673b91e01bf90
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-08-18 22:01:14 +0200

    Added CustomVOCSegmentation

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,2 +1,2 @@
-from .voc import VOCSegmentation, VOCSegmentationIncremental
+from .voc import VOCSegmentation, VOCSegmentationIncremental, CustomVOCSegmentation
 from .ade import AdeSegmentation, AdeSegmentationIncremental
