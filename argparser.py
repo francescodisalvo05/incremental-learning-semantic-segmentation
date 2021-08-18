d0b20565ecc9955187855e1af727ae508f309c1a
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-08-18 22:04:23 +0200

    Added ResNet18

--- argparser.py
+++ argparser.py
@@ -127,7 +127,7 @@ def get_argparser():
 
     # Model Options
     parser.add_argument("--backbone", type=str, default='resnet50',
-                        choices=['resnet50', 'resnet101'], help='backbone for the body (def: resnet50)')
+                        choices=['resnet18','resnet50', 'resnet101'], help='backbone for the body (def: resnet50)')
     parser.add_argument("--output_stride", type=int, default=16,
                         choices=[8, 16], help='stride for the backbone (def: 16)')
     parser.add_argument("--no_pretrained", action='store_true', default=True,
