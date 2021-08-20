2a368dbb88f5b2c1020e952b1e91df47900f615d
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-08-20 09:21:18 +0200

    Changed RandomRotation

--- run.py
+++ run.py
@@ -41,7 +41,7 @@ def get_dataset(opts):
     """
     train_transform = transform.Compose([
         transform.RandomResizedCrop(opts.crop_size, (0.5, 2.0)),
-        transform.RandomRotation(degrees=45),
+        transform.RandomRotation(degrees=15),
         #transform.RandomHorizontalFlip(),
         transform.ToTensor(),
         transform.Normalize(mean=[0.485, 0.456, 0.406],
